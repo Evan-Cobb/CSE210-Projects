10a4dc4ef06404faaaa3b7055332669079a8d922
Evan-Cobb <evan.cobb2004@gmail.com>
2026-01-10 17:01:00 -0700

    Completed assignments 1&2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        Console.WriteLine("What is your grade percentage?");
+        string gradePercentage = Console.ReadLine();
+        if (double.Parse(gradePercentage) >= 90)
+        {
+            letter = "A";
+        }
+        else if (double.Parse(gradePercentage) >= 80)
+        {
+            letter = "B";
+        }
+        else if (double.Parse(gradePercentage) >= 70)
+        {
+            letter = "C";
+        }
+        else if (double.Parse(gradePercentage) >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (letter == "A")
+        {
+            Console.WriteLine($"You got an {letter}!");
+        }
+        else
+        {
+            Console.WriteLine($"You got a {letter}!");            
+        }
+        if (double.Parse(gradePercentage) >= 70)
+        {
+            Console.WriteLine("That means you passed! Great job!");
+        }
+        else
+        {
+            Console.WriteLine("That means you failed this time,but BYU-I has resources to help you out if you need them, you've got this!");
+        }
     }
 }
