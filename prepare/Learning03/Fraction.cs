3c6558af433f7145e281281fd2d7472658c075c9
Evan-Cobb <evan.cobb2004@gmail.com>
2026-01-20 13:09:24 -0600

    completed preper-Learning03

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,63 @@
+using System;
+
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        // Default to 1/1
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _numerator = wholeNumber;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        Setdenominator(denominator);
+    }
+
+    public void Setdenominator(int denominator)
+    {
+        if (denominator != 0)
+        {
+            _denominator = denominator;
+        }
+        else
+        {
+            _denominator = 1;
+        }
+    }
+
+    public void Setnumerator(int numerator)
+    {
+        _numerator = numerator;
+    }
+
+    public int Getnumerator()
+    {
+        return _numerator;
+    }
+    public int Getdenominator()
+    {
+        return _denominator;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_numerator}/{_denominator}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / (double)_denominator;
+    }
+}
